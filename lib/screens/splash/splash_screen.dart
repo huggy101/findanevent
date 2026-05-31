6b586f6eb1844a97b21e418b670554d71d61df2d
Hugh P <hugh_prosser@hotmail.com>
2026-05-31 14:32:50 +0100

    add splash screen

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 2), () {
+      if (!mounted) return;
+      context.go('/welcome');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white,
+        child: Center(
+          child: Image.asset(
+            'assets/icon.png',
+            width: 120,
+            height: 120,
+          ),
+        ),
+      ),
+    );
+  }
+}
